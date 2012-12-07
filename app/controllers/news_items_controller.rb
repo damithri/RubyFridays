d2c8b5bb92b21680fd934e7332b697dfcb9a584c
teldio-operations <operations@teldio.com>
2012-12-07 15:18:17 -0500

    Formatted news items controller.

--- app/controllers/news_items_controller.rb
+++ app/controllers/news_items_controller.rb
@@ -1,37 +1,42 @@
 # hello this is a change
-
 class NewsItemsController < ApplicationController
   def index
     @news_items=NewsItem.all
     @title="News Items"
     @news_item=NewsItem.new #added this here to reference partial and to invoke the new action because we're calling it from the index page now.
-      end
-   def new
-     @news_item=NewsItem.new
-   end
-   def create
-     @news_item=NewsItem.create(params[:news_item])
-     if @news_item.save
+  end
+
+  def new
+    @news_item=NewsItem.new
+  end
+
+  def create
+    @news_item=NewsItem.create(params[:news_item])
+    if @news_item.save
       redirect_to "/news_items"
-     else
+    else
       redirect_to "/news_items/new"
-     end
-   end
-   def edit
-     @news_item=NewsItem.find(params[:id])
-   end
-   def update
-     @news_item=NewsItem.find(params[:id])
-     @news_item.update_attributes(params[:news_item])
-     @news_item.save
-     redirect_to "/news_items"
-   end
-   def destroy
-     @news_item=NewsItem.find(params[:id])
-     @news_item.destroy
-     redirect_to "/news_items"
-   end
-   def show
-     @news_item=NewsItem.find(params[:id])
-     end
- end
+    end
+  end
+
+  def edit
+    @news_item=NewsItem.find(params[:id])
+  end
+
+  def update
+    @news_item=NewsItem.find(params[:id])
+    @news_item.update_attributes(params[:news_item])
+    @news_item.save
+    redirect_to "/news_items"
+  end
+
+  def destroy
+    @news_item=NewsItem.find(params[:id])
+    @news_item.destroy
+    redirect_to "/news_items"
+  end
+
+  def show
+    @news_item=NewsItem.find(params[:id])
+  end
+end
